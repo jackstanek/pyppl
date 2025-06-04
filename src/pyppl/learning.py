aeb0b94e8dcf379c2054f90b2f35f8b729c904b6
Jack Stanek <stanekjack@gmail.com>
2025-06-04 16:29:04 -0500

    Use average negative log likelihood
    Replace negative log likelihood as it scales with dataset size, making
    large datasets unwieldly to deal with

--- src/pyppl/learning.py
+++ src/pyppl/learning.py
@@ -4,15 +4,15 @@ from pyppl import ast
 from pyppl.params import ParamVector
 
 
-def negative_log_likelihood(
+def avg_negative_log_likelihood(
     prog: ast.ExpressionNode, params: ParamVector, data: list[ast.PureNode]
 ) -> float:
     """Compute the negative log-likelihood of a collection of data."""
     env = ast.Environment(params)
-    return -sum(math.log(prog.infer(env, d)) for d in data)
+    return -sum(math.log(prog.infer(env, d)) for d in data) / len(data)
 
 
-def negative_log_likelihood_gradient(
+def avg_negative_log_likelihood_gradient(
     prog: ast.ExpressionNode, params: ParamVector, data: list[ast.PureNode]
 ) -> ParamVector:
     """Compute the gradient of the negative log-likelhood function.
@@ -27,9 +27,9 @@ def negative_log_likelihood_gradient(
         and the parameters
     """
     env = ast.Environment(params)
-    grad = sum(prog.gradient(env, val) / prog.infer(env, val) for val in data)
-    if grad == 0:
-        raise ValueError("empty training set")
+    grad = sum(prog.gradient(env, val) / prog.infer(env, val) for val in data) / len(data)
+    if isinstance(grad, (float, int)):
+        raise ValueError("empty training set or type error")
     return grad
 
 
@@ -37,14 +37,14 @@ def optimize(
     prog: ast.ExpressionNode,
     data: list[ast.PureNode],
     epochs: int = 100,
-    learning_rate: float = 0.001,
+    learning_rate: float = 0.01,
 ) -> ParamVector:
     """Optimize parameters to maximize the likelihood of the training set"""
     params = ParamVector.random(prog.params)
     for epoch in range(epochs):
-        nll = negative_log_likelihood(prog, params, data)
+        nll = avg_negative_log_likelihood(prog, params, data)
         print(f"epoch: {epoch}; nll: {nll}")
-        grad = negative_log_likelihood_gradient(prog, params, data)
+        grad = avg_negative_log_likelihood_gradient(prog, params, data)
         params += learning_rate * grad
 
     return params
